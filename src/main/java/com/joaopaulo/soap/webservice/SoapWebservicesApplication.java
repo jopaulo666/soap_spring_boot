62b43e60876a410c068927fef07d64f9e3415b27
João Paulo <jp_cbc@hotmail.com>
2019-06-18 23:41:45 -0300

    Criação do projeto Webservices SOAP com Spring Boot, JPA - administração de clientes

---
+++ src/main/java/com/joaopaulo/soap/webservice/SoapWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.joaopaulo.soap.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaopaulo/soap/webservice/SoapWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaopaulo.soap.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
